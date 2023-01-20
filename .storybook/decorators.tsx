abc217e381e5e81ead0f51efa8589b4e2c8243eb
mgbld <112904350+mgbld@users.noreply.github.com>
2023-01-19 18:33:24 -0600

    Implement theme switcher

--- .storybook/decorators.tsx
+++ .storybook/decorators.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { DecoratorFn } from '@storybook/react';
 import { GlobalStyle } from '../src/styles/GlobalStyle';
-import { lightTheme } from '../src/styles/theme';
+import { lightTheme, darkTheme } from '../src/styles/theme';
 
-const withTheme: DecoratorFn = (StoryFn) => (
-  <ThemeProvider theme={lightTheme}>
-    <GlobalStyle />
-    {StoryFn()}
-  </ThemeProvider>
-);
+const withTheme: DecoratorFn = (StoryFn, context) => {
+  const theme = context.globals.theme;
+  const storyTheme = theme === 'dark' ? darkTheme : lightTheme;
+  console.log(storyTheme);
+  return (
+    <ThemeProvider theme={storyTheme}>
+      <GlobalStyle />
+      {StoryFn()}
+    </ThemeProvider>
+  );
+};
 
 export const globalDecorators = [withTheme];
